1f601d83c50373e92ffc6c75760b5e23acdf5d38
Alex Walker <alensw11@gmail.com>
2015-12-26 01:46:46 +0200

    added view logic to the navigation for logged in users

--- cards/views.py
+++ cards/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from .models import Card
 
 def hello_world(request):
     return render(request, "cards/hello_world.html")
 
 def card_view(request, id):
-    id = 2
-    return render(request, "cards/card_template.html", {"id":id})
+    id = 1
+    query_set = Card.objects.get(pk=1)
+    return render(request, "cards/card_template.html", {"query_set":query_set})
 
