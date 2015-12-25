5d259c3fcc31511358ef492cf211165efc5d1406
Alex Walker <alensw11@gmail.com>
2015-12-26 00:05:07 +0200

    added nav and jquery

--- cards/admin.py
+++ cards/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Card
+from .models import Deck
 
-# Register your models here.
+
+admin.site.register(Card)
+admin.site.register(Deck)
--- cards/urls.py
+++ cards/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls import url, include
 from . import views
-from django.conf.urls.static import static
-from django.conf import settings
 
 
 urlpatterns = [
   url(r'^home/', views.hello_world, name="hello_world"),
+  url('^card/(?P<id>\d+)/$', views.card_view, name="card"),
 
-
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
