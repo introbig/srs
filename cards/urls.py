1f012ccda12b22f589bb885f2137782e5b554327
Alex Walker <alensw11@gmail.com>
2015-12-26 05:08:33 +0200

    deleting cards works

--- cards/urls.py
+++ cards/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
   url('^card_template/(?P<id>\d+)/$', views.card_view, name="card"),
   url('^decks_template', views.decks_view, name="decks"),
-
+  url('^edit_cards_template/(?P<id>\d+)/$', views.edit_cards_view, name="edit_cards"),
+  url('^delete_card_template/(?P<id1>\d+)/(?P<id2>\d+)/$', views.delete_card_view, name="delete_card")
 ]
--- cards/views.py
+++ cards/views.py
@@ -14,3 +14,20 @@ def card_view(request, id):
 def decks_view(request):
     query_set = Deck.objects.all()
     return render(request, "cards/decks_template.html", {"query_set": query_set})
+
+
+def edit_cards_view(request, id):
+    query_set = Card.objects.filter(deck__exact=id)
+    deck_name = Deck.objects.get(pk=id).name
+    deck_id = Deck.objects.get(pk=id).id
+    return render(request, "cards/edit_cards_template.html", {"query_set": query_set, "deck_name": deck_name, "deck_id":deck_id})
+
+
+def delete_card_view(request, id1, id2):
+    model_instance = Card.objects.get(pk = id1)
+    model_instance.delete()
+
+    query_set = Card.objects.filter(deck__exact=id2)
+    deck_name = Deck.objects.get(pk=id2).name
+    deck_id = Deck.objects.get(pk=id2).id
+    return render(request, "cards/edit_cards_template.html", {"query_set": query_set, "deck_name": deck_name, "deck_id":deck_id})
